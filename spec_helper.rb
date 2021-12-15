643b965fe324fdb08a1977708bbe406375b1a008
Shariq Torres <shariq.torres@lob.com>
2021-12-15 14:45:32 -0800

    updates

--- spec_helper.rb
+++ spec_helper.rb
@@ -1,3 +1,15 @@
+
+   
+$:.unshift File.expand_path("../lib", File.dirname(__FILE__))
+
+require "simplecov"
+require "coveralls"
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
+SimpleCov.start
 require "fob"
 require "minitest/spec"
 require "minitest/pride"
