c8580783cfef07702f474ef110f0ad8d8449554a
Shariq Torres <shariq.torres@lob.com>
2021-12-15 14:58:06 -0800

    updated

--- spec/fob_spec.rb
+++ spec/fob_spec.rb
@@ -4,6 +4,7 @@ describe Fob do
 
   it "should return the client object for the valid version" do
     lob = Fob::Client.new(api_key: "test", api_version: "2014-11-25")
-    lob.must_be_kind_of(Lob::Client)
+    lob.must_be_kind_of(Fob::Client)
     assert lob.config[:api_version] == "2014-11-25"
   end
+end
