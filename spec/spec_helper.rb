24a1cd34efd47cb294da63b0760c474b5f8a1d1b
Shariq Torres <shariq.torres@lob.com>
2021-12-15 15:31:17 -0800

    updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,12 @@ $:.unshift File.expand_path("../lib", File.dirname(__FILE__))
 
 require "simplecov"
 require "coveralls"
+require 'simplecov-lcov'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-])
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov::Formatter::LcovFormatter.config.lcov_file_name = 'lcov.info'
+
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start
 require "fob"
 require "minitest/spec"
